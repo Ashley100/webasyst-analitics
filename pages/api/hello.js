5641aaef9b3fc210db0c46e18dd2e811194abbbc
piter <tor20099@gmail.com>
2021-01-01 19:34:48 +0200

    Changed settings for Heroku deployments.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,21 +1,21 @@
-import Google from '../../backend/parsers/google/google.js';
+// import Google from '../../backend/parsers/google/google.js';
 
 export default async (req, res) => {
 
   const { data } = req.body;
 
-  console.log(data.text);
-
-  const parseGoogle = new Google ({
-    // findSite: 'avito.ru',
-    findSite: data.text,
-    utilsList: ['position', 'competitors', 'indexation'],
-  });
-
-  let parse = await parseGoogle.parse();
-
-  console.log("hello.js => ", parse);
+  // console.log(data.text);
+  //
+  // const parseGoogle = new Google ({
+  //   findSite: data.text,
+  //   utilsList: ['position', 'competitors', 'indexation'],
+  // });
+  //
+  // let parse = await parseGoogle.parse();
+  //
+  // console.log("hello.js => ", parse);
 
   res.statusCode = 200;
-  res.json({ name: 'John Doe', parse: parse[1] })
+  // res.json({ name: 'John Doe', parse: parse[1] })
+  res.json({ name: 'John Doe' })
 }
